76e9bba3195a636aba57b25c3ae0bf3eb6c5db8d
simon <grytysek@gmail.com>
2015-03-28 16:02:14 +0100

    CsvFileIterator fix

--- src/Stopsopa/UtilsBundle/Lib/CsvIterator.php
+++ src/Stopsopa/UtilsBundle/Lib/CsvIterator.php
@@ -5,14 +5,31 @@ namespace Stopsopa\UtilsBundle\Lib;
 use Stopsopa\UtilsBundle\Lib\Standalone\UtilFilesystem;
 use Iterator;
 
+/**
+ *
+for ($it->rewind(); $it->valid(); $it->next()) {
+    echo $it->key().' - '.$it->current();
+}
+ */
 class CsvFileIterator implements Iterator {
     protected $file;
     protected $key = 0;
     protected $current;
+    protected $valid;
+    protected $options;
 
-    public function __construct($file) {
+    public function __construct($file, $options = array()) {
         UtilFilesystem::checkFile($file);
         $this->file = fopen($file, 'r');
+        if (is_array($options)) {
+            $this->options = array_merge(array(
+                'length'    => 0,
+                'delimiter' => ',',
+                'enclosure' => '"',
+                'escape'    => '\\',
+                'skip'      => 0 // skip lines
+            ), $options);
+        }
     }
 
     public function __destruct() {
@@ -21,12 +38,30 @@ class CsvFileIterator implements Iterator {
 
     public function rewind() {
         rewind($this->file);
-        $this->current = fgetcsv($this->file);
         $this->key = 0;
+
+        if ($this->options['skip']) {
+            for ( $i = 0 ; $i < $this->options['skip'] ; ++$i )
+                $this->next();
+        }
+
+        $this->next();
+    }
+
+    public function next() {
+        $this->valid    = !feof($this->file);
+        $this->current  = fgetcsv(
+            $this->file,
+            $this->options['length'],
+            $this->options['delimiter'],
+            $this->options['enclosure'],
+            $this->options['escape']
+        );
+        $this->key++;
     }
 
     public function valid() {
-        return !feof($this->file);
+        return $this->valid;
     }
 
     public function key() {
@@ -35,10 +70,5 @@ class CsvFileIterator implements Iterator {
 
     public function current() {
         return $this->current;
-    }
-
-    public function next() {
-        $this->current = fgetcsv($this->file);
-        $this->key++;
     }
 }
