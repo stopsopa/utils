4c03592c27cd385d5c9115eeaf028db031ddb53c
szymon <szymon.dzialowski@absolvent.pl>
2015-08-14 10:29:40 +0200

    upload

--- src/Stopsopa/UtilsBundle/Lib/DownloadFile.php
+++ src/Stopsopa/UtilsBundle/Lib/DownloadFile.php
@@ -2,7 +2,10 @@
 
 namespace Stopsopa\UtilsBundle\Lib;
 
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use DateTime;
 
 class DownloadFile
 {
@@ -15,7 +18,7 @@ class DownloadFile
      *
      * @return Response
      */
-    public static function download($file, $filename = null)
+    public static function downloadDie($file, $filename = null)
     {
         if (!$filename) {
             $filename = pathinfo($file, PATHINFO_BASENAME);
@@ -24,12 +27,61 @@ class DownloadFile
         $size = filesize($file);
 
         header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename="'.$filename.'"');
+        header("Content-Disposition: attachment; filename=" . urlencode($file));
+        header("Content-Description: File Transfer ".urlencode($file));
         header('Last-Modified: '.date('D, d M Y H:i:s \G\M\T'));
         header("Content-Length: $size");
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 
         readfile($file);
 
         return new Response();
+    }
+    /**
+     * Man http://symfony.com/doc/current/components/http_foundation/introduction.html#serving-files
+     * g(When sending a file, you must add a Content-Disposition header to your response symfony2)
+     * @param string $file
+     * @param string $filename
+     */
+    public static function downloadStatic($file, $headers = array()) {
+
+        if (!is_array($headers)) {
+            $headers = array();
+        }
+
+        $headers['Content-Type'] = 'application/octet-stream';
+
+        $respone = new BinaryFileResponse(
+            $file,
+            $status = 200,
+            $headers,
+            $public = true,
+            $contentDisposition = ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            $autoEtag = true,
+            $autoLastModified = true
+        );
+
+        return $respone;
+    }
+    public static function downloadGenerated($data, $filename, $headers = array()) {
+
+        $response = new Response();
+
+        $disposition = $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            pathinfo($filename, PATHINFO_BASENAME)
+        );
+
+        $response->headers->set('Content-Disposition', $disposition);
+        $response->headers->set('Content-Type', 'application/octet-stream');
+
+        return $response->setCache(array(
+            'last_modified' => new DateTime(),
+            'max_age'       => 0,
+            's_maxage'      => 0,
+            'private'       => false,
+            'public'        => true,
+        ))->setContent($data);
     }
 }
