fff39d98dab7db8643f1832f753826db41bb4a4e
Simon <stopsopa@users.noreply.github.com>
2017-01-17 09:26:47 +0000

    Update _localtest.php

--- src/Stopsopa/UtilsBundle/Resources/public/standalone-service/_localtest.php
+++ src/Stopsopa/UtilsBundle/Resources/public/standalone-service/_localtest.php
@@ -3,7 +3,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$ch = curl_init('http://www.legenhit.com/yui-service/service.php?mode=js');
+$ch = curl_init('http://www.legenhit.com/yui-service/service.php?mode=js');        
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT , 10); 
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, array(
   'data' => file_get_contents('../src/Core/UberBundle/Resources/public/js/jquery-jtemplates-0.8.js'),
