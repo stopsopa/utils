eaffff7e8d151bc7bb674b498f6c7fa1d864cf5b
Simon <stopsopa@users.noreply.github.com>
2017-08-10 15:55:59 +0100

    Update CsvFileIterator.php
    added transform method to CsvFileIterator

--- src/Lib/CsvFileIterator.php
+++ src/Lib/CsvFileIterator.php
@@ -5,36 +5,6 @@ namespace Stopsopa\UtilsBundle\Lib;
 use Stopsopa\UtilsBundle\Lib\Standalone\UtilFilesystem;
 use Iterator;
 
-/*
-
-
-$file = '2017-08-10-dump.txt';
-
-$csv = new CsvFileIterator($file, array(
-    'delimiter' => "\t"
-));
-
-$row = 0;
-$headers = array();
-
-foreach ($csv as $item) {
-
-    if ($row === 0) {
-        $headers = $item;
-    }
-    else {
-        $item = array_combine($headers, $item);
-        print_r($item);
-    }
-
-    if ($row > 5) {
-
-        break;
-    }
-
-    $row += 1;
-}
-*/
 class CsvFileIterator implements Iterator
 {
     protected $file;
@@ -42,13 +12,19 @@ class CsvFileIterator implements Iterator
     protected $current;
     protected $valid;
     protected $options;
+    protected $transform;
 
-    public function __construct($file, $options = array())
+    /**
+     * CsvFileIterator constructor.
+     * @param $file
+     * @param array $options
+     * @param callable|null $transform - optional function to process row before splitting it by "str_getcsv"
+     */
+    public function __construct($file, $options = array(), Callable $transform = null)
     {
         UtilFilesystem::checkFile($file);
-
+        
         $this->file = fopen($file, 'r');
-
         if (is_array($options)) {
             $this->options = array_merge(array(
                 'length' => 0,
@@ -58,51 +34,69 @@ class CsvFileIterator implements Iterator
                 'skip' => 0, // skip lines
             ), $options);
         }
-    }
 
+        $this->transform = $transform;
+    }
     public function __destruct()
     {
         fclose($this->file);
     }
-
     public function rewind()
     {
         rewind($this->file);
-
         $this->key = 0;
-
         if ($this->options['skip']) {
             for ($i = 0; $i < $this->options['skip']; ++$i) {
                 $this->next();
             }
         }
-
         $this->next();
     }
-
     public function next()
     {
         $this->valid = !feof($this->file);
-        $this->current = fgetcsv(
-            $this->file,
-            $this->options['length'],
-            $this->options['delimiter'],
-            $this->options['enclosure'],
-            $this->options['escape']
-        );
+
+        if ($this->transform) {
+
+            if ($this->options['length']) {
+                $this->current = fgets(
+                    $this->file,
+                    $this->options['length']
+                );
+            }
+            else {
+                $this->current = fgets($this->file);
+            }
+
+            $this->current = str_getcsv(
+                call_user_func(
+                    $this->transform,
+                    $this->current
+                ),
+                $this->options['delimiter'],
+                $this->options['enclosure'],
+                $this->options['escape']
+            );
+        }
+        else {
+            $this->current = fgetcsv(
+                $this->file,
+                $this->options['length'],
+                $this->options['delimiter'],
+                $this->options['enclosure'],
+                $this->options['escape']
+            );
+        }
         $this->key += 1;
     }
-
     public function valid()
     {
         return $this->valid;
     }
-
     public function key()
     {
         return $this->key;
     }
-
     public function current()
     {
         return $this->current;
