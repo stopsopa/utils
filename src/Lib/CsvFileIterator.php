bd14a3f78bca93ee2ed4db5bbf2690c66b1c5aeb
Simon <stopsopa@users.noreply.github.com>
2017-08-10 13:16:36 +0100

    Update CsvFileIterator.php

--- src/Lib/CsvFileIterator.php
+++ src/Lib/CsvFileIterator.php
@@ -5,6 +5,36 @@ namespace Stopsopa\UtilsBundle\Lib;
 use Stopsopa\UtilsBundle\Lib\Standalone\UtilFilesystem;
 use Iterator;
 
+/**
+
+
+$file = '2017-08-10-dump.txt';
+
+$csv = new CsvFileIterator($file, array(
+    'delimiter' => "\t"
+));
+
+$row = 0;
+$headers = array();
+
+foreach ($csv as $item) {
+
+    if ($row === 0) {
+        $headers = $item;
+    }
+    else {
+        $item = array_combine($headers, $item);
+        print_r($item);
+    }
+
+    if ($row > 5) {
+
+        break;
+    }
+
+    $row += 1;
+}
+*/
 class CsvFileIterator implements Iterator
 {
     protected $file;
