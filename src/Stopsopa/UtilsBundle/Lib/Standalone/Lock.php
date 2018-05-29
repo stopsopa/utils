720f45122d0cb3cc0e504713f56d717690630a30
Szymon Dzialowski <stopsopa@users.noreply.github.com>
2018-05-29 15:27:45 +0100

    Update Lock.php

--- src/Stopsopa/UtilsBundle/Lib/Standalone/Lock.php
+++ src/Stopsopa/UtilsBundle/Lib/Standalone/Lock.php
@@ -1,12 +1,9 @@
 <?php
 
-namespace Stopsopa\UtilsBundle\Lib\Standalone;
+namespace CoreBundle\Libs;
 
 use Exception;
 
-/**
- * Stopsopa\UtilsBundle\Lib\Lock.
- */
 class Lock
 {
     protected $file;
@@ -16,7 +13,13 @@ class Lock
         $this->file = $file;
 
         if (!file_exists($this->file)) {
-            throw new Exception("File '".$this->file."' doesn't exist");
+
+            file_put_contents($this->file, '', FILE_APPEND);
+        }
+
+        if (!file_exists($this->file)) {
+
+            throw new Exception("Lock: File '".$this->file."' doesn't exist and can't be created");
         }
 
         $class = $this;
@@ -39,36 +42,28 @@ class Lock
     public function lock($seconds = null, $throw = true)
     {
         $this->ch = fopen($this->file, 'r');
-
         if ($seconds = abs((int) $seconds)) {
             $time = time();
             while ((time() - $time) < $seconds) {
                 if (flock($this->ch, LOCK_EX | LOCK_NB)) {
                     return true;
                 }
-
                 sleep(1);
             }
-
             if ($throw) {
                 throw new Exception("Can't lock critical section on file '".$this->file."'");
             }
-
             return false;
         }
-
         flock($this->ch, LOCK_EX);
-
         return $this;
     }
     public function unlock()
     {
         $this->ch or ($this->ch = fopen($this->file, 'r'));
-
         flock($this->ch, LOCK_UN);
         fclose($this->ch);
         $this->ch = null;
-
         return $this;
     }
 }
